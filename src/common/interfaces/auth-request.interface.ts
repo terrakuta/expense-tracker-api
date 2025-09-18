76847929a862eed921b056ad35f46d7d651fb1fd
Pavlo <silencer1793@gmail.com>
2025-09-19 01:05:44 +0300

    Add expense-tracker-api

--- src/common/interfaces/auth-request.interface.ts
+++
@@ -1,6 +0,0 @@
-import type { Request } from 'express';
-import type { JwtPayload } from './jwt.interface';
-
-export interface AuthRequest extends Request {
-  user?: JwtPayload;
-}
--- src/expenses/dto/category.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateCategoryDto {
-  @IsString({ message: 'Name should be a string' })
-  @IsNotEmpty({ message: 'Name is required' })
-  name: string;
-}
